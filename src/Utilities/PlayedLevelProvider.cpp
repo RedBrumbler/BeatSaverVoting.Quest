a25755ec2e69cc090336a92d3ba84057ff77e00e
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-05-09 14:13:26 +0200

    Add logging for when getting the level

--- src/Utilities/PlayedLevelProvider.cpp
+++ src/Utilities/PlayedLevelProvider.cpp
@@ -1,16 +1,21 @@
 #include "Utilities/PlayedLevelProvider.hpp"
 #include "UI/VotingUI.hpp"
 #include "GlobalNamespace/GameplayCoreSceneSetupData.hpp"
+#include "logging.hpp"
 
 DEFINE_TYPE(BeatSaverVoting::Utilities, PlayedLevelProvider);
 
 namespace BeatSaverVoting::Utilities {
     void PlayedLevelProvider::ctor(Zenject::DiContainer* container) {
         auto votingUI = UI::VotingUI::get_instance();
-        if (!votingUI) return;
+        if (!votingUI) {
+            WARNING("Voting UI instance is null! can't set last level");
+            return;
+        }
 
         auto sceneSetupData = container->TryResolve<GlobalNamespace::GameplayCoreSceneSetupData*>();
         if (!sceneSetupData) {
+            WARNING("Couldn't get scene setup data, setting level to null!");
             votingUI->lastLevel = nullptr;
             return;
         }
